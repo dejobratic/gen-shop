4080dbe6e76a4b0ce18b4c73fa36b58ae444ce4b
dejobratic <dejobratic@gmail.com>
2021-05-12 21:07:52 +0200

    added invoicing app layer;

---
+++ GenShop.Invoicing.App/UseCases/ICommand.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+
+namespace GenShop.Invoicing.App.UseCases
+{
+    public interface ICommand
+    {
+        Task Execute();
+    }
+
+    public interface ICommand<T>
+    {
+        Task<T> Execute();
+    }
+}
